d88de221f20ca10fdbb6f224754d5ab07702105c
Ricardo Herrera <ricardo@pe-amini1.avanticape.avantica.avanticatec.net>
2014-08-05 10:54:09 -0500

    .

--- JenkinsTestProject/JenkinsTestProject/avtAppDelegate.m
+++ JenkinsTestProject/JenkinsTestProject/avtAppDelegate.m
@@ -3,7 +3,7 @@
 //  JenkinsTestProject
 //
 //  Created by Ricardo Herrera on 8/4/14.
-//  Copyright (c) 2014 Avantica. All rights reserved.
+//  Copyright (c) 2014 Avantica. All rights reserved..
 //
 
 #import "avtAppDelegate.h"
